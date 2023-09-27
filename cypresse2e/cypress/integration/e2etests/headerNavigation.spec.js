11845a92d25d349170f92e4cf57fe083a866b299
jessica-lau <jessica.lau2@gmail.com>
2023-09-27 16:19:36 -0400

    edit formatting in code

--- cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
+++ cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
@@ -2,6 +2,7 @@ import Header from "../../pages/elements/headerNavigation";
 
 describe("Level99 header section", () => {
   const header = new Header();
+
   context("User views content in header navigation", () => {
     beforeEach(() => {
       header.visit();
