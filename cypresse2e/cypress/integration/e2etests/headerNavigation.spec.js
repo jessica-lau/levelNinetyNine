ffdb012dd844e6bcfb6e09b063b7b77a138aebc9
jessica-lau <jessica.lau2@gmail.com>
2023-09-27 16:18:59 -0400

    add beforeEach visit command

--- cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
+++ cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
@@ -2,4 +2,9 @@ import Header from "../../pages/elements/headerNavigation";
 
 describe("Level99 header section", () => {
   const header = new Header();
+  context("User views content in header navigation", () => {
+    beforeEach(() => {
+      header.visit();
+    });
+  });
 });
