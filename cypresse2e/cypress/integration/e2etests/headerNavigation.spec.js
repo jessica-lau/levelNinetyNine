7a751ac7ddc7cfe2aada84990a4c23e03d2e4f4b
jessica-lau <jessica.lau2@gmail.com>
2023-10-02 15:08:57 -0400

    add test scripts for dropdown home, explore, event planning, about, faq

--- cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
+++ cypresse2e/cypress/integration/e2etests/headerNavigation.spec.js
@@ -12,6 +12,26 @@ describe("Level99 header section", () => {
       header.getLogo().should("be.visible").click();
     });
 
+    it("Home dropdown is clickable", () => {
+      cy.get('a[href="/"]').click();
+    });
+
+    it("Explore dropdown is clickable", () => {
+      cy.get('a[href="/explore/entertainment"').click();
+    });
+
+    it("Event Planning dropdown is clickable", () => {
+      cy.get('a[href="/events"').click();
+    });
+
+    it("About dropdown is clickable", () => {
+      cy.get('a[href="/about/faq/what-is"').click();
+    });
+
+    it("FAQ dropdown is clickable", () => {
+      cy.get('a[href="/about/faq"').click();
+    });
+
     it("Tickets button is clickable", () => {
       cy.get('a[href="https://ticketing.level99.com"]').click();
     });
