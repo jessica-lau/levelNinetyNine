328e05cc6e4511d2b0f03bea066d72c72bf21e4d
jessica-lau <jessica.lau2@gmail.com>
2023-10-04 13:13:50 -0400

    add logo and social media elements

--- cypresse2e/cypress/pages/elements/footer.js
+++ cypresse2e/cypress/pages/elements/footer.js
@@ -1,4 +1,9 @@
 class Footer {
+  logo = ".block logo";
+  facebookSocial = ".social > .icon fb";
+  instagramSocial = ".social > .icon ig";
+  twitterSocial = ".social > .icon tw";
+
   constructor() {}
 
   visit() {
