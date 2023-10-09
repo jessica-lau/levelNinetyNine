df04991d6886fbab307f80c853fbc912ce20c27e
jessica-lau <jessica.lau2@gmail.com>
2023-10-09 14:37:19 -0400

    add beforeEach hook

--- cypresse2e/cypress/integration/e2etests/footer.spec.js
+++ cypresse2e/cypress/integration/e2etests/footer.spec.js
@@ -2,4 +2,10 @@ import Footer from "../../pages/elements/footer";
 
 describe("Level99 footer section", () => {
   const footer = new Footer();
+
+  context("User view content in footer", () => {
+    beforeEach(() => {
+      footer.visit();
+    });
+  });
 });
