d14e3d0d36e3e768f3188f7df9163a6f7d5965bf
jessica-lau <jessica.lau2@gmail.com>
2023-10-10 12:09:14 -0400

    add test script for instagram and twitter

--- cypresse2e/cypress/integration/e2etests/footer.spec.js
+++ cypresse2e/cypress/integration/e2etests/footer.spec.js
@@ -15,5 +15,13 @@ describe("Level99 footer section", () => {
     it("Facebook social media icon is visible and clickable", () => {
       footer.getFbSocial().should("be.visible").click();
     });
+
+    it("Instagram social media icon is visible and clickable", () => {
+      footer.getIgSocial().should("be.visible").click();
+    });
+
+    it("Twitter social media icon is visible and clickable", () => {
+      footer.getTwitterSocial().should("be.visible").click();
+    });
   });
 });
