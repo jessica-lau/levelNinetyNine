234f6a5813f447a0167d79d66402036fd0891956
jessica-lau <jessica.lau2@gmail.com>
2023-12-07 13:32:30 -0500

    add before each hook

--- cypresse2e/cypress/integration/e2etests/homepage.spec.js
+++ cypresse2e/cypress/integration/e2etests/homepage.spec.js
@@ -2,4 +2,8 @@ import HomePage from "../../pages/elements/homepage";
 
 describe("Level99 homepage", () => {
   const homepage = new HomePage();
+
+  beforeEach(() => {
+    homepage.visit();
+  });
 });
